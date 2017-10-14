36f77e2250db3c4543a4eda60747173a1fbfd604
chrisziegler <chrisrziegler@gmail.com>
2017-10-13 23:38:55 -0700

    added handlebar (hbs) templating and partials for header and footer

--- server.js
+++ server.js
@@ -1,25 +1,49 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.use((req, res, next) => {
+    //toString method creates a nicely formatted time stamp
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.')
+        }
+    })
+    next();
+}); 
+//set-up handler for http get request, our url is root of app
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
 //set-up handler for http get request, our url is root of app
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        welcomeText: 'Welcome to the home of Some Page!',
-        currentYear: new Date().getFullYear()
+        welcomeText: 'Welcome to my webserver'
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     });
 });
 
